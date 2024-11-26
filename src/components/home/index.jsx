01e1dc40c1135766497bc8de0f28d676436d58b0
EunchaeKo <kec0130@gmail.com>
2024-11-27 00:28:15 +0900

    Fix case error

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,7 +3,7 @@ import Hero from "./Hero";
 import Banners from "./Banners";
 import FreeLectures from "./FreeLectures";
 import Courses from "./Courses";
-import BootCamp from "./Bootcamp";
+import BootCamp from "./BootCamp";
 
 export default function Home() {
   return (
